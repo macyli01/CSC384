e9556cb89c6c2f52e7bd01a75d7ea340d79975b0
Vincent Lee <vincentleest@Vincents-MacBook-Pro.local>
2014-03-24 19:51:55 -0400

    Created competition code

--- lab2/heuristic_c4leevin.pl
+++ lab2/heuristic_c4leevin.pl
@@ -11,11 +11,11 @@
 
 % If you decide to participate in the competition, you should
 % submit a file named "heuristic_<id>.pl", where <id> should
-% be your cdf id. For example, a student John Doe with id "c9jdoe" would
-% submit the file named "heuristic_c9jdoe.pl".
+% be your cdf id. For example, a student John Doe with id "c4leevin" would
+% submit the file named "heuristic_c4leevin.pl".
 % 
 % This file should contain a predicate named "<id>_h/2"
-% (John would name it "c9jdoe_h"). This predicate
+% (John would name it "c4leevin_h"). This predicate
 % can rely on the functions you are required to implement (eg. tie, winner,
 % nextState etc). Any helper predicates
 % should also be included in the heuristic file.
@@ -33,23 +33,26 @@
 % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %-------------------------
-% Surname: Doe
-% First Name: John
-% Student Number: 123456789
+% Surname: Lee
+% First Name: Vincent
+% Student Number: 997454419
 
 % Helper predicates
-c9jdoe_max(C1, C2, C) :- C1 >= C2, !, C=C1.
-c9jdoe_max(C1, C2, C2) :- C1 < C2.
-
-c9jdoe_count([], 0).
-c9jdoe_count([E|L], C) :- length(E, C1),
-     c9jdoe_count(L, C2), c9jdoe_max(C1, C2, C).
+c4leevin_max(C1, C2, C) :- C1 >= C2, !, C=C1.
+c4leevin_max(C1, C2, C2) :- C1 < C2.
 
 % The actual heuristic
-c9jdoe_h(State, Val) :- terminal(State), !, Val=42.
-c9jdoe_h(State, Val) :- c9jdoe_count(State, Val2), Val is Val2-4.
+c4leevin_h(State, Val) :- terminal(State), !, Val=0.
+c4leevin_h(State, Val) :- 
+	( terminal(State) ->
+		moves(1, State, MvList1),
+		moves(2, State, MvList2),
+		length(MvList1, L1),
+		length(MvList2, L2),
+		Val is L1 - L2	
+	; Val is 0).
 
 % The bounds
-c9jdoe_lowerBound(-3).
-c9jdoe_upperBound(300).
+c4leevin_lowerBound(-36).
+c4leevin_upperBound(36).
 
